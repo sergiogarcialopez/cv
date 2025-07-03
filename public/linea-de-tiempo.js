57d8ad511eda38c6febd20fb2b0806cbbd650423
Sergio García López <109881039+sergiogarcialopez@users.noreply.github.com>
2025-07-02 23:57:51 -0600

    Update linea-de-tiempo.js
    Cambio en etiquetas y nueva entrada año 2006-2009

--- public/linea-de-tiempo.js
+++ public/linea-de-tiempo.js
@@ -13,12 +13,13 @@ function drawChart() {
     var ultimaActualizacion = new Date(2025, 6, 24);
     dataTable.addRows([
         ['1', 'Tiempo de vida desde el nacimiento hasta la última actualización de la presente línea de tiempo', new Date(1999, 2, 13), ultimaActualizacion],
-        ['2', 'Diplomado Photoshop Nivel Avanzado', new Date(2014, 0, 1), new Date(2014, 11, 1)],
+        ['11', 'Inglés Básico', new Date(2006, 0, 1), new Date(2009, 11, 1)],
         ['7', 'Diplomado Diseño de Páginas Web', new Date(2014, 0, 1), new Date(2014, 11, 1)],
+        ['2', 'Diplomado Photoshop Nivel Avanzado', new Date(2014, 0, 1), new Date(2014, 11, 1)],
         ['3', 'Certificación HSK 1', new Date(2015, 0, 1), new Date(2015, 11, 1)],
-        ['4', 'Certificación Microsoft Office Specialist for Office PowerPoint 2010', new Date(2016, 0, 1), new Date(2016, 11, 1)],
+        ['4', 'Certificación MS Office PowerPoint 2010', new Date(2016, 0, 1), new Date(2016, 11, 1)],
         ['5', 'Certificación DELF A1', new Date(2017, 0, 1), new Date(2017, 11, 1)],
-        ['6', 'Técnico en Sistemas computacionales y desarrollo de software', new Date(2018, 0, 1), new Date(2018, 11, 1)],
+        ['6', 'Técnico en Sist. comp. y desarr. de soft.', new Date(2018, 0, 1), new Date(2018, 11, 1)],
         ['8', 'Curso Full Stack Developer Java', new Date(2023, 0, 1), new Date(2024, 1, 19)],//19-02-2024
         ['9', 'Certificación Huawei Cloud HCCDA', new Date(2024, 2, 1), new Date(2024, 2, 8)],//19-02-2024
         ['10', 'Licenciatura en Ingeniería en Tecnologías y sistemas de informacíon (en curso)', new Date(2022, 0, 1), ultimaActualizacion]
